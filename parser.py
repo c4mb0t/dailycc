6742f5bc10f566a99bb45ee120496945d9b02396
Scott Myers <semyers@gmail.com>
2013-02-17 19:12:24 -0800

    Initial checkin of parser code

---
+++ parser.py
@@ -0,0 +1,21 @@
+import csv
+import datetime
+from dateutil import relativedelta
+
+totals = dict()
+cutoffs = {'MC': 20, 'VISA': 23}
+today = datetime.date.today()
+with open('test.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        rawdate = row[0]
+        date = datetime.datetime.strptime(rawdate, "%Y/%m/%d").date()
+        card = row[1]
+        charge = float(row[2])
+        if not card in totals:
+            totals[card] = 0
+        endtime = datetime.date(today.year, today.month, cutoffs[card])
+        starttime = endtime - relativedelta.relativedelta(months=1)
+        if date > starttime and date < endtime:
+            totals[card] = totals[card] + charge
+print totals
