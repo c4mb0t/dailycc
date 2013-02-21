8718cf5ec2c15a12202d18e3c63cbe7c6cddbca4
Scott Myers <semyers@gmail.com>
2013-02-20 19:09:00 -0800

    Updated csv format

--- parser.py
+++ parser.py
@@ -7,9 +7,11 @@ cutoffs = {'MC': 20, 'VISA': 23}
 today = datetime.date.today()
 with open('test.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
+    # Skip first line (it's a header)
+    next(reader)
     for row in reader:
         rawdate = row[0]
-        date = datetime.datetime.strptime(rawdate, "%Y/%m/%d").date()
+        date = datetime.datetime.strptime(rawdate, "%m/%d/%Y").date()
         card = row[1]
         charge = float(row[2])
         if not card in totals:
